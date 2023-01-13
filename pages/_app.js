4cf1d0d6b4e7d2135e2f0b38477fcafeb358180f
Jozef Babos <jozef.babos11@gmail.com>
2023-01-13 09:43:34 +0100

    feature/autologin

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/globals.scss";
 import Layout from "../components/layout/layout";
+import useFetch from "../hooks/use-fetch";
 import { LanguageContext, CurrentUserContext } from "../AppContext";
 import { LANGUAGES } from "../languagesObjects";
 import { SLOVAK, CZECH, ENGLISH } from "../languagesObjects";
@@ -11,7 +12,19 @@ function MyApp({ Component, pageProps }) {
    const [currentUser, setCurrentUser] = useState(null);
    const [isLoading, setIsLoading] = useState(true);
 
+   const { error, sendRequest, isAuth } = useFetch();
+
    useEffect(() => {
+      const token = localStorage.getItem("dinhotoken");
+      const options = { method: "GET", token };
+
+      const transformData = (data) => {
+         if (Number.isNaN(Number(data))) {
+            setCurrentUser(data);
+         }
+      };
+      sendRequest("/autologin", options, transformData);
+
       let timer = setTimeout(() => {
          setApplanguage(() => {
             for (const language in LANGUAGES) {
@@ -21,6 +34,7 @@ function MyApp({ Component, pageProps }) {
             }
             return TRANSLATIONS["CZECH"];
          });
+
          setIsLoading(false);
       }, 100);
 
@@ -34,9 +48,11 @@ function MyApp({ Component, pageProps }) {
    return (
       <LanguageContext.Provider value={{ applanguage, setApplanguage }}>
          <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
-            <Layout>
-               <Component {...pageProps} />
-            </Layout>
+            {isAuth && (
+               <Layout>
+                  <Component {...pageProps} />
+               </Layout>
+            )}
          </CurrentUserContext.Provider>
       </LanguageContext.Provider>
    );
