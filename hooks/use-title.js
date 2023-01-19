2b60f720e6c0fb49c7e85f74d9e001003c6fd25b
Jozef Babos <jozef.babos11@gmail.com>
2023-01-19 13:42:32 +0100

    bugFix/npm_build_hook-rulz

--- hooks/use-title.js
+++ hooks/use-title.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-const useTitle = (side, opponent, start, index) => {
-   if (index === undefined)
-      return !side
-         ? `Chelsea - ${opponent} (N)`
-         : side === 1
-         ? `Chelsea - ${opponent}`
-         : `${opponent} - Chelsea`;
-
-   return !side
-      ? `${index}. Chelsea - ${opponent} (N) ${start}`
+const setTitle = (side, opponent, start, index) => {
+  if (index === undefined)
+    return !side
+      ? `Chelsea - ${opponent} (N)`
       : side === 1
-      ? `${index}. Chelsea - ${opponent} ${start}`
-      : `${index}. ${opponent} - Chelsea ${start}`;
+      ? `Chelsea - ${opponent}`
+      : `${opponent} - Chelsea`;
+
+  return !side
+    ? `${index}. Chelsea - ${opponent} (N) ${start}`
+    : side === 1
+    ? `${index}. Chelsea - ${opponent} ${start}`
+    : `${index}. ${opponent} - Chelsea ${start}`;
 };
 
-export default useTitle;
+export default setTitle;
